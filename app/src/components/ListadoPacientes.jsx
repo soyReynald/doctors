db3b58c2fdd459228c74f14586bb10b4846876c0
Reynald Rodriguez <rrodriguez@aldereca.com>
2022-01-26 17:21:55 -0400

    [U] Conditional text

--- app/src/components/ListadoPacientes.jsx
+++ app/src/components/ListadoPacientes.jsx
@@ -5,6 +5,8 @@ const ListadoPacientes = ({ pacientes }) => {
     const generatedKey = () => Math.random().toString(36).substring(2) + Date.now().toString(36);
 
   return (
+    pacientes.length > 0 ?(
+    <>
     <div className="md:w-1/2 md:h-screen overflow-y-scroll">
       <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
       <p className="text-xl mt-5 text-center">
@@ -22,6 +24,15 @@ const ListadoPacientes = ({ pacientes }) => {
         />
       ))}
     </div>
+    </>)
+    :
+    <div className="md:w-1/2 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+      <p className="text-xl mt-5 text-center">
+        Agrega tus pacientes y {""}
+        <span className="text-indigo-600 font-bold">aparecen en este lugar</span>
+      </p>
+      </div>
   );
 };
 
