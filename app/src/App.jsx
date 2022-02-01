e5c2fe562bcfb71aaae12e7bf068eaa4441288b2
Reynald Ramirez <reynaldramirez@hotmail.com>
2022-02-01 13:24:54 -0400

    [U] Changes in the effect and getting ready to Edit the patience

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,27 @@
-import {useState} from 'react';
+import { useState } from "react";
 import Formulario from "./components/Formulario";
 import Header from "./components/Header";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
-    
     const [pacientes, setPacientes] = useState([]);
+    const [paciente, setPaciente] = useState({});
 
     return (
         <>
             <div className="container mx-auto pt-20">
                 <Header />
                 <div className="mt-12 md:flex w-full justify-between">
-                    <Formulario 
+                    <Formulario
                         pacientes={pacientes}
                         setPacientes={setPacientes}
+                        paciente={paciente}
+                        setPaciente={setPaciente}
                     />
-                    <ListadoPacientes pacientes={pacientes} />
+                    <ListadoPacientes
+                        pacientes={pacientes}
+                        setPaciente={setPaciente}
+                    />
                 </div>
             </div>
         </>
